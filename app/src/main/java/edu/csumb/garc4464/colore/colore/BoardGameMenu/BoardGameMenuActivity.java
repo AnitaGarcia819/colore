315e306c7f044b1340efc0cb6d70cc28382fc6d0
Benito Sanchez <sanchezbenitog@gmail.com>
2016-08-07 11:24:28 -0700

    work in progress

--- app/src/main/java/edu/csumb/garc4464/colore/colore/BoardGameMenu/BoardGameMenuActivity.java
+++ app/src/main/java/edu/csumb/garc4464/colore/colore/BoardGameMenu/BoardGameMenuActivity.java
@@ -1,15 +1,58 @@
 package edu.csumb.garc4464.colore.colore.BoardGameMenu;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import javax.inject.Inject;
+
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
 import edu.csumb.garc4464.colore.R;
 
-public class BoardGameMenuActivity extends AppCompatActivity {
+public class BoardGameMenuActivity extends Activity implements BoardGameMenuLayout.Listener{
+
+    @Inject BoardGameMenuLayout mBoardGameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_board_game);
+//        setContentView(R.layout.activity_board_game);
+
+         BoardGameMenuComponent component = DaggerBoardGameMenuActivity_BoardGameMenuComponent.builder()
+                .boardGameMenuModule(new BoardGameMenuModule(this, this)).build();
+        component.inject(this);
+    }
+
+    @Override
+    public void onNewGameClicked() {
+        Toast.makeText(this, "New Game", Toast.LENGTH_SHORT).show();
+    }
+
+    @Component(modules = {BoardGameMenuModule.class})
+    interface BoardGameMenuComponent {
+        void inject(BoardGameMenuActivity boardGameMenuActivity);
+    }
+
+    @SuppressWarnings("unused")
+    @Module
+    static class BoardGameMenuModule {
+
+        private Context mContext;
+        private BoardGameMenuLayout.Listener mBoardGameLayoutListener;
+
+        public BoardGameMenuModule(Context context, BoardGameMenuLayout.Listener listener) {
+            mContext = context;
+            mBoardGameLayoutListener = listener;
+        }
+
+        @NonNull
+        @Provides
+        BoardGameMenuLayout providesBoardGameMenuLayout() {return new BoardGameMenuLayout(mContext, mBoardGameLayoutListener);}
     }
 }
--- app/src/main/java/edu/csumb/garc4464/colore/colore/BoardGameMenu/BoardGameMenuLayout.java
+++ app/src/main/java/edu/csumb/garc4464/colore/colore/BoardGameMenu/BoardGameMenuLayout.java
@@ -1,7 +1,38 @@
 package edu.csumb.garc4464.colore.colore.BoardGameMenu;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import edu.csumb.garc4464.colore.R;
+
 /**
  * Created by anitagarcia on 7/23/16.
  */
-public class BoardGameMenuLayout {
+public class BoardGameMenuLayout extends FrameLayout {
+
+    @BindView(R.id.new_game) Button mNewGameButton;
+    private Listener mListener;
+
+    public BoardGameMenuLayout(Context context, BoardGameMenuLayout.Listener listener) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.options_menu_dialog, this);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.new_game)
+    public void onNewGameClick() {
+        Toast.makeText(getContext(), "test", Toast.LENGTH_SHORT).show();
+        mListener.onNewGameClicked();
+    }
+
+    interface Listener {
+        void onNewGameClicked();
+    }
 }
